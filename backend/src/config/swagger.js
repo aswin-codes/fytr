c4ba49c781a38148275f282923432f902ce909db
Aswinraaj P S <aswinraaj.ps@gmail.com>
2025-12-26 16:54:20 +0530

    fix : config for swagger

--- backend/src/config/swagger.js
+++ backend/src/config/swagger.js
@@ -14,6 +14,10 @@ const options = {
                 url: 'http://localhost:3000',
                 description: 'Development server',
             },
+            {
+              url: 'https://backendfytr.vercel.app',
+              description: 'Production server'
+            }
         ],
         components: {
             securitySchemes: {
