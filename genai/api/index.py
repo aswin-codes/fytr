79523c82e2a2eb30b7fb249445aefbc7689a7b80
Aswinraaj P S <aswinraaj.ps@gmail.com>
2026-01-01 17:57:11 +0530

    pls work:

--- genai/api/index.py
+++ genai/api/index.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException, Form
+from fastapi import FastAPI, File, UploadFile, HTTPException, Form, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from http.server import BaseHTTPRequestHandler
